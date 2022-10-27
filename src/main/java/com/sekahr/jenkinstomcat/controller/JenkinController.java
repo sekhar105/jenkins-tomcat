de1afa67d77618784f4d4d5ca8f2735a389ad059
sekhar105 <repaka99999>
2022-10-27 15:29:36 +0530

    added responsebody annotaion

--- src/main/java/com/sekahr/jenkinstomcat/controller/JenkinController.java
+++ src/main/java/com/sekahr/jenkinstomcat/controller/JenkinController.java
@@ -2,12 +2,14 @@ package com.sekahr.jenkinstomcat.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@ResponseBody
 public class JenkinController {
 
 	
-	@RequestMapping("/")
+	@RequestMapping("/add")
 	public String welcome() {
 		
 		return "welcome bro";
