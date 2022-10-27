375d6e575945138503f26c1b7c6e9cd33d747e51
sekhar105 <repaka99999>
2022-10-27 15:51:12 +0530

    new comment added

--- src/main/java/com/sekahr/jenkinstomcat/controller/JenkinController.java
+++ src/main/java/com/sekahr/jenkinstomcat/controller/JenkinController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class JenkinController {
 
 	
-	@RequestMapping("/add")
+	@RequestMapping("/")
 	public String welcome() {
 		
 		return "welcome";
